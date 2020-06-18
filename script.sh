9254202e4bc5521dffa983842c95415da2f86925
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-06-18 13:21:01 +0300

    Fix Drone config

--- script.sh
+++ script.sh
@@ -13,6 +13,9 @@ if test -n "$PACKAGE_LOCK_CHANGED";then
   echo $PLUGIN_GIT_SSH_KEY > ssh_key
   chmod 0600 ssh_key
 
+  echo $DRONE_GIT_SSH_URL > foo
+  cat foo
+
   git remote add remote $DRONE_GIT_SSH_URL
 
   git config core.sshCommand 'ssh -i ssh_key -o StrictHostKeyChecking=no'
@@ -23,7 +26,7 @@ if test -n "$PACKAGE_LOCK_CHANGED";then
 
   npm version patch
 
-  git push --follow-tags remote $DRONE_COMMIT_BRANCH
+  git push -v --follow-tags remote $DRONE_COMMIT_BRANCH
 else
   echo "No changes"
 fi
