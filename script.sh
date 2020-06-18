9a66f604ae1a2f4a46c4dce5e538200d39fd22ce
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-06-18 12:55:17 +0300

    Fix Drone config

--- script.sh
+++ script.sh
@@ -10,18 +10,13 @@ PACKAGE_LOCK_CHANGED=`git diff --name-only HEAD | grep package-lock.json`
 if test -n "$PACKAGE_LOCK_CHANGED";then
   echo "Package lock has changed. Saving changes"
 
-  echo "$GIT_SSH_KEY" > ssh_key
+  echo "$PLUGIN_GIT_SSH_KEY" > ssh_key
   chmod 0600 ssh_key
   du -b ssh_key
 
-  #git remote add origin $DRONE_GIT_SSH_URL
-
   git config core.sshCommand 'ssh -i ssh_key -o StrictHostKeyChecking=no'
-  echo 'set ssh command'
-  git config user.name $GIT_USER_NAME
-  echo 'set user name'
-  git config user.email $GIT_USER_EMAIL
-  echo 'set user email'
+  git config user.name $PLUGIN_GIT_USER_NAME
+  git config user.email $PLUGIN_GIT_USER_EMAIL
 
   git commit -a -m 'Update dependencies'
 
