d17efd36a653114a3995162e56af1fc5e79453e4
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-06-22 10:00:47 +0300

    test

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Docker image for Drone to publish NPM dependency updates to Git
 
+## Settings
+- git_user_name: natlibfi-melinda-automation
+- git_user_email: 65649125+natlibfi-melinda-automation@users.noreply.github.com
+- git_ssh_key: Must be Base64 encoded
+- git_ssh_key: Must be Base64 encoded
+
 ## License and copyright
 
 Copyright (c) 2020 **University Of Helsinki (The National Library Of Finland)**
--- script.sh
+++ script.sh
@@ -10,10 +10,10 @@ PACKAGE_LOCK_CHANGED=`git diff --name-only HEAD | grep package-lock.json`
 if test -n "$PACKAGE_LOCK_CHANGED";then
   echo "Package lock has changed. Saving changes"
 
-  echo $PLUGIN_GIT_SSH_KEY |base64 -d > ssh_key
+  echo $PLUGIN_GIT_SSH_KEY | base64 -d > ssh_key
   chmod 0600 ssh_key
 
-  echo $PLUGIN_GIT_GPG_KEY | gpg --import -q  
+  echo $PLUGIN_GIT_GPG_KEY | base64 -d | gpg --import -q  
 
   git remote add remote $DRONE_GIT_SSH_URL
   
